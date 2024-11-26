981de57bc8499377e0d16ce3d88a5ee08a1a1cb2
Orlando Betancourth <orlando@uledger.io>
2024-11-25 19:50:54 -0600

    fix vite-config error 2

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,11 @@ import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 
+
 // https://vite.dev/config/
 export default ({mode}) => {
   process.env = {...process.env, ...loadEnv(mode, process.cwd())};
+  console.log("Env:", process.env);
   return defineConfig({
     base: process.env.VITE_BASE || '/',
     plugins: [react()],
