da198bea99e2ec8730cc90830a9225bae23094d9
Orlando Betancourth <orlando@uledger.io>
2024-11-25 19:46:42 -0600

    fix: adding base route name

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,13 @@ import './App.css'
 import ListPage from './pages/List';
 import DetailPage from './pages/Detail';
 import {BrowserRouter as Router, Routes,  Route, } from 'react-router-dom';
+
+const BASE_DIR = import.meta.env.BASE_URL;
+
 function App() {
   // "JSX" -> nomenclatura (facebook | meta) pseudoHTML
   return (
-    <Router>
+    <Router basename={BASE_DIR}>
       <Routes>
           <Route path='/' element={(<ListPage/>)} />
           <Route path='/detail/:pokemonId' element={(<DetailPage/>)} />
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,12 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
+
 // https://vite.dev/config/
-export default defineConfig({
-  base: '/pw2_pockemon_demo_202403/',
-  plugins: [react()],
-})
+export default returnConfig = ({mode}) => {
+  process.env = {...process.env, ...loadEnv(mode, process.cwd())};
+  return defineConfig({
+    base: process.env.VITE_BASE || '/',
+    plugins: [react()],
+  })
+}
